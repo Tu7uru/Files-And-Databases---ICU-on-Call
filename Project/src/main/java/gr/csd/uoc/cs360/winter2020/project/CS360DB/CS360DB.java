0a536ea118c5414bf235b926b8ad2db09c35b663
Apostolos <csd3799@csd.uoc.gr>
2021-01-07 12:43:11 +0200

    establishing connection & creating servlet

--- Project/src/main/java/gr/csd/uoc/cs360/winter2020/project/CS360DB/CS360DB.java
+++ Project/src/main/java/gr/csd/uoc/cs360/winter2020/project/CS360DB/CS360DB.java
@@ -5,10 +5,28 @@
  */
 package gr.csd.uoc.cs360.winter2020.project.CS360DB;
 
+import java.sql.*;
+
 /**
  *
  * @author Apostolos
  */
 public class CS360DB {
+    private static final String URL = "jdbc:mysql://localhost";
+    private static final String DATABASE = "CS360";
+    private static final int PORT = 3306;
+    private static String UNAME = new String("root");
+    private static String PASSWD = new String("");
 
+    /**
+     * Attempts to establish a database connection Using mariadb
+     *
+     * @return a connection to the database
+     * @throws SQLException
+     * @throws java.lang.ClassNotFoundException
+     */
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection(URL + ":" + PORT + "/" + DATABASE, UNAME, PASSWD);
+    }
 }
